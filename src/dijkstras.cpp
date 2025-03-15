5550a8144014fa4f834673a332eb3f9a6d23e270
NoNathan17 <nathanong1217@gmail.com>
2025-03-15 02:27:45 +0000

    debugging

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -63,10 +63,10 @@ vector<int> extract_shortest_path(const vector<int>& distances, const vector<int
 
 // Function to print the shortest path and its total distance
 void print_path(const vector<int>& path, int total) {
-    if (path.empty()) {
-        cout << "\nTotal cost is " << INF << endl;        
-        return;
-    }
+    // if (path.empty()) {
+    //     cout << "No path found." << endl;
+    //     return;
+    // }
 
     for (int i = 0; i < path.size(); ++i) {
         cout << path[i];
